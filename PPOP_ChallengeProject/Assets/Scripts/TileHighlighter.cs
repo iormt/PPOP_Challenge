0781407e6430570aeae198488ff697380715db88
Ramiro Travieso <ragtre@gmail.com>
2022-02-22 01:56:33 -0300

    Added TileHighlighter to namespace Assets.Scripts.MapGeneration

--- PPOP_ChallengeProject/Assets/Scripts/TileHighlighter.cs
+++ PPOP_ChallengeProject/Assets/Scripts/TileHighlighter.cs
@@ -2,52 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileHighlighter : MonoBehaviour
+namespace Assets.Scripts.MapGeneration
 {
-    [SerializeField] private Color Normal;
-    [SerializeField] private Color PathHighlighted;
-    [SerializeField] private Color EndsHighlighted;
-
-    [SerializeField] private Vector3 NormalScale;
-    [SerializeField] private Vector3 HighlightedScale;
-
-    private Renderer _renderer;
-    private MaterialPropertyBlock _propBlock;
-
-    private bool highlighted = false;
-
-    public void Init()
+    public class TileHighlighter : MonoBehaviour
     {
-        _propBlock = new MaterialPropertyBlock();
-        _renderer = GetComponent<Renderer>();
-    }
+        [SerializeField] private Color Normal;
+        [SerializeField] private Color PathHighlighted;
+        [SerializeField] private Color EndsHighlighted;
 
-    public void HighlightTile(bool isEnd)
-    {
-        // Get the current value of the material properties in the renderer.
-        _renderer.GetPropertyBlock(_propBlock);
-        // Assign our new value.
-        if (isEnd)
+        [SerializeField] private Vector3 NormalScale;
+        [SerializeField] private Vector3 HighlightedScale;
+
+        private Renderer _renderer;
+        private MaterialPropertyBlock _propBlock;
+
+        public void Init()
         {
-            _propBlock.SetColor("_Color", EndsHighlighted);
+            _propBlock = new MaterialPropertyBlock();
+            _renderer = GetComponent<Renderer>();
         }
-        else
+
+        public void HighlightTile(bool isEnd)
         {
-            _propBlock.SetColor("_Color", PathHighlighted);
+            // Get the current value of the material properties in the renderer.
+            _renderer.GetPropertyBlock(_propBlock);
+            // Assign our new value.
+            if (isEnd)
+            {
+                _propBlock.SetColor("_Color", EndsHighlighted);
+            }
+            else
+            {
+                _propBlock.SetColor("_Color", PathHighlighted);
+            }
+            //_propBlock.SetColor("_Color", Color.Lerp(Normal, PathHighlighted, (Mathf.Sin(Time.time * Speed + Offset) + 1) / 2f));
+            // Apply the edited values to the renderer.
+            _renderer.SetPropertyBlock(_propBlock);
+            transform.localScale = HighlightedScale;
         }
-        //_propBlock.SetColor("_Color", Color.Lerp(Normal, PathHighlighted, (Mathf.Sin(Time.time * Speed + Offset) + 1) / 2f));
-        // Apply the edited values to the renderer.
-        _renderer.SetPropertyBlock(_propBlock);
-        transform.localScale = HighlightedScale;
-    }
-    public void DehighlightTile()
-    {   
-        // Get the current value of the material properties in the renderer.
-        _renderer.GetPropertyBlock(_propBlock);
-        // Assign our new value.
-         _propBlock.SetColor("_Color", Normal);
-        // Apply the edited values to the renderer.
-        _renderer.SetPropertyBlock(_propBlock);
-        transform.localScale = NormalScale;
+        public void DehighlightTile()
+        {
+            // Get the current value of the material properties in the renderer.
+            _renderer.GetPropertyBlock(_propBlock);
+            // Assign our new value.
+            _propBlock.SetColor("_Color", Normal);
+            // Apply the edited values to the renderer.
+            _renderer.SetPropertyBlock(_propBlock);
+            transform.localScale = NormalScale;
+        }
     }
 }
